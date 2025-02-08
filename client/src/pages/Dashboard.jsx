dc1024a8ea54ef99fbd64fd8f0586ab6b0553c1a
itspandey22 <piyushpandey2200@gmail.com>
2025-02-09 00:40:22 +0530

    phirse hogya

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -73,16 +73,7 @@ const Dashboard = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="min-h-screen bg-gray-100">
-      <Navbar /> {/* Navbar will take full width if coded correctly */}
-      <main className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        <Card title="Total Users" value="1,250" color="#3b82f6" />
-        <Card title="Sales Today" value="$4,500" color="#10b981" />
-        <Card title="Active Sessions" value="350" color="#f59e0b" />
-      </main>
-    </div>
-=======
+    <div className=""><Navbar/>
     <main className="p-6 grid grid-cols-1 gap-10">
       
       {/* Enrolled Exams Section */}
@@ -136,7 +127,7 @@ const Dashboard = () => {
       </section>
 
     </main>
->>>>>>> fcc2598e252c6652c73f973df567b1a30b04c4b3
+    </div>
   );
 };
 
