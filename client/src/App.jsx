faaea6925ac092e99310a7058a436591a48bda06
Rishi2531 <ris.mis.rt22@dypatil.edu>
2025-02-08 22:13:51 +0530

    `Added Home page component and imported it into App component`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Home from './pages/Home'
 
 function App() { 
 
   return (
     <>
       <h1>Testing</h1>
+      <Home/>
     </>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+
+
+function Home() {
+  return (
+    <div>
+        <Navbar/>
+        <NewsItem/>
+        <div>
+            <h1>Home</h1>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
